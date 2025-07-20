9976fb841496e4171d094af3459ee90965acc230
Nahom32 <senaynahom00@gmail.com>
2025-07-20 11:08:11 +0300

    feat: added matrix multiplication function

---
+++ matrix_multiplication.m
@@ -0,0 +1,24 @@
+function C = matrix_multiply(A, B)
+    % Get dimensions
+    [m, n] = size(A);
+    [n2, p] = size(B);
+    
+    % Check compatibility
+    if n ~= n2
+        error('Matrix dimensions are incompatible for multiplication.');
+    end
+    
+    % Initialize result matrix with zeros
+    C = zeros(m, p);
+    
+    % Perform multiplication
+    for i = 1:m
+        for j = 1:p
+            sum_val = 0;
+            for k = 1:n
+                sum_val += A(i, k) * B(k, j);
+            end
+            C(i, j) = sum_val;
+        end
+    end
+end
